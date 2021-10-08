47fda21a0d92a78ea598e94ca48c4b31b666dfba
Darryn <darryn.mclachlan@experian.com>
2021-10-08 09:26:37 +0200

    gulp updated

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,33 +1,13 @@
 var gulp    = require('gulp');
-var eslint  = require('gulp-eslint');
 var babel   = require('gulp-babel');
 var mocha   = require('gulp-mocha');
 var del     = require('del');
 
-gulp.task('clean', function (cb) {
-    del('lib', cb);
-});
+function clean() {
+    return del('lib');
+}
 
-gulp.task('lint', function () {
-    return gulp
-        .src([
-            'src/**/*.js',
-            'test/**/*.js',
-            'Gulpfile.js'
-        ])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
-});
-
-gulp.task('build', ['clean', 'lint'], function () {
-    return gulp
-        .src('src/**/*.js')
-        .pipe(babel( { optional: ['runtime'] } ))
-        .pipe(gulp.dest('lib'));
-});
-
-gulp.task('test', ['build'], function () {
+function test() {
     return gulp
         .src('test/**.js')
         .pipe(mocha({
@@ -35,19 +15,14 @@ gulp.task('test', ['build'], function () {
             reporter: 'spec',
             timeout:  typeof v8debug === 'undefined' ? 2000 : Infinity // NOTE: disable timeouts in debug
         }));
-});
+}
 
-gulp.task('preview', ['build'], function () {
-    var buildReporterPlugin = require('testcafe').embeddingUtils.buildReporterPlugin;
-    var pluginFactory       = require('./lib');
-    var reporterTestCalls   = require('./test/utils/reporter-test-calls');
-    var plugin              = buildReporterPlugin(pluginFactory);
+function build() {
+    return gulp
+        .src('src/**/*.js')
+        .pipe(babel( { optional: ['runtime'] } ))
+        .pipe(gulp.dest('lib'));
+}
 
-    console.log();
-
-    reporterTestCalls.forEach(function (call) {
-        plugin[call.method].apply(plugin, call.args);
-    });
-
-    process.exit(0);
-});
+exports.build = gulp.series(clean, build);
+exports.test = gulp.series(build, test);
